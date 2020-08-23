a156e25a165f1d6083ac32abd02f7b0fb68e7769
deborahtrez <deborahtrez12@gmail.com>
2020-08-23 21:13:14 +0300

    resolving netlify issue

---
+++ build/precache-manifest.029df212f4a0f95391e0b4ccbdd9f141.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36bcb4f36258df956973c9bea974fffc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da4f50428c6482dcf123",
+    "url": "/static/css/main.9cbdb04f.chunk.css"
+  },
+  {
+    "revision": "26c422e099442fe3884a",
+    "url": "/static/js/2.6caf9e36.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6caf9e36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da4f50428c6482dcf123",
+    "url": "/static/js/main.d60e4e06.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
